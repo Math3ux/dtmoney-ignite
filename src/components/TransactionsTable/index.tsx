77057a38f363fba4049862eff20901f0bba1afbb
Matheus <matheusleidow@gmail.com>
2024-06-08 15:31:07 -0300

    Created and Styled Component Transacions Table

---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./styles";
+
+export function TransactionsTable(){
+    return (
+        <Container>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Titulo</th>
+                        <th>Valor</th>
+                        <th>Categoria</th>
+                        <th>Data</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Desenvolvimento de Website</td>
+                        <td className="deposit">R$12.000,00</td>
+                        <td>Desenvolvimento</td>
+                        <td>07/06/2024</td>
+                    </tr>
+                    <tr>
+                        <td>Aluguel</td>
+                        <td className="withdraw">- R$1.100,00</td>
+                        <td>Casa</td>
+                        <td>01/06/2024</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Container>
+    )
+}
