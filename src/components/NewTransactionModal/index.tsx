639c5326d2bf468ccd77eede30ac38aa158502df
Matheus <matheusleidow@gmail.com>
2024-06-09 17:16:19 -0300

    Finished Styled RadioBox

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import ReactModal from "react-modal";
-import { Container, TransactionTypeContainer } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from "react";
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen,onRequestClose}:Readonly<NewTransactionModalProps>){
+    const [type, setType] = useState('deposit')
+
     return (
     <ReactModal isOpen={isOpen} onRequestClose={onRequestClose} overlayClassName='react-modal-overlay' className='react-modal-content'>
         <button type="button" onClick={onRequestClose} className="react-modal-close"><img src={closeImg} alt="Fechar Modal" /></button>
@@ -19,8 +22,8 @@ export function NewTransactionModal({isOpen,onRequestClose}:Readonly<NewTransact
             <input placeholder='Valor' type="number" />
 
             <TransactionTypeContainer>
-                <button type='button'><img src={incomeImg} alt="Entrada" /><span>Entrada</span></button>
-                <button type='button'><img src={outcomeImg} alt="Saída" /><span>Saída</span></button>
+                <RadioBox type='button' onClick={() => {setType('deposit')}} isActive={type === 'deposit'} activeColor='green'><img src={incomeImg} alt="Entrada" /><span>Entrada</span></RadioBox>
+                <RadioBox type='button' onClick={() => {setType('withdraw')}} isActive={type === 'withdraw'} activeColor='red'><img src={outcomeImg} alt="Saída" /><span>Saída</span></RadioBox>
             </TransactionTypeContainer>
 
             <input placeholder='Categoria' type="text" />
